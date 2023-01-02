5ad148daef6eb3eba10a3b55ca1a6580d49b6c21
pradip-148 <pradip.gutte@searce.com>
2023-01-02 13:55:39 +0530

    adding cloudbuild updated 2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import webapp2
+# import webapp2
 
-class HelloWebapp2(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello, this response is from webapp2!. We are testing for ci/cd pipelines with GCP')
+# class HelloWebapp2(webapp2.RequestHandler):
+#     def get(self):
+#         self.response.write('Hello, this response is from webapp2!. We are testing for ci/cd pipelines with GCP')
 
-app = webapp2.WSGIApplication([
-    ('/', HelloWebapp2),
-], debug=True)
+# app = webapp2.WSGIApplication([
+#     ('/', HelloWebapp2),
+# ], debug=True)
